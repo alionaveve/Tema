862e6ce5d96e62b388289445cfa3dad27d42d7cd
alionaveve <nushkaromanovici@gmail.com>
2023-06-08 18:48:26 +0300

    Tema

--- ex8.py
+++ ex8.py
@@ -1,16 +1,16 @@
 temperatura=int(input("Introdu temperatura de afara: "))
 
-if temperatura <=-40 or temperatura<=-10:
+if temperatura <=-40 and temperatura<=-10:
     print(f"Vreme extrem de rece!")
-if  temperatura <=-10 or temperatura<=0:
+elif  temperatura <=-10 and temperatura<=0:
         print(f"Vreme foarte rece!")
-if temperatura <=0 or temperatura <=10:
+elif temperatura <=0 and temperatura <=10:
         print(f"Vreme rece!")
-elif temperatura <=10 or temperatura<=20:
+elif temperatura <=10 and temperatura<=20:
             print(f"Vreme normala")
-elif temperatura <=20 or temperatura<=30:
+elif temperatura <=20 and temperatura<=30:
                 print(f"Vreme calda")
-elif temperatura <=30 or temperatura <=40:
+elif temperatura <=30 and temperatura <=40:
     print(f"Este foarte cald")
 if temperatura>=40:
         print(f"Este extrem de cald")
